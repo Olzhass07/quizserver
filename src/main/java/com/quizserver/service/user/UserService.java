90e2e92890fba7252c41d8f4477015504a2cb336
Olzhass07 <bolatulyolzhas@gmail.com>
2025-10-20 21:13:19 +0500

    do 6

---
+++ src/main/java/com/quizserver/service/user/UserService.java
@@ -0,0 +1,10 @@
+package com.quizserver.service.user;
+
+import com.quizserver.enteties.User;
+
+public interface UserService {
+
+    User createUser(User user);
+
+    Boolean hasUserWithEmail(String email);
+}
