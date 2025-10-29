73c3f72b089782f08661a5a02c5cd32d93fc5175
Arlan <zangazyarlan369@gmail.com>
2025-10-29 15:53:07 +0500

    16

---
+++ src/main/java/com/quizserver/repository/TestRepository.java
@@ -0,0 +1,9 @@
+package com.quizserver.repository;
+
+import com.quizserver.enteties.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, Long> {
+}
